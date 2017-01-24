aad95ee55927bfbd760ae4d82b8b576bdbe178cf
Pedro Martinez <pmartinez1085@gmail.com>
2017-01-24 17:02:38 +0100

    parrilla

---
+++ resources/views/parrilla.blade.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
+<style>
+	.row,.col {
+		border:1px black solid;
+		background: cornflowerblue;
+		text-align: center;
+		
+	}
+	.container {
+		border: 1px green solid;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+		margin: 0 auto;
+		height:100%;
+	}
+	
+</style>
+<body>
+
+<div class="container">
+	<div class="row">
+		<div class="col">1</div>
+		<div class="col">2</div>
+		<div class="col">3</div>
+		<div class="col">4</div>
+		<div class="col">5</div>
+		<div class="col">6</div>
+	</div>
+	
+	<div class="row">
+		<div class="col">1</div>
+		<div class="col">2</div>
+	</div>
+	
+	<div class="row">
+		<div class="col">1</div>
+		<div class="col">2</div>
+		<div class="col">3</div>
+	</div>
+
+	<div class="row">
+		<div class="col">1</div>
+		<div class="col">2</div>
+		<div class="col">3</div>
+	</div>
+
+	<div class="row">
+		<div class="col">1</div>
+		<div class="col">2</div>
+	</div>
+
+	<div class="row">
+		<div class="col">1</div>
+		<div class="col">2</div>
+		<div class="col">3</div>
+		<div class="col">4</div>
+		<div class="col">5</div>
+		<div class="col">6</div>
+	</div>
+	
+</div>
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Route::group(['middleware' => 'auth'], function () {
 
     //Please do not remove this if you want adminlte:route and adminlte:link commands to works correctly.
     #adminlte_routes
+    Route::get('parrilla', 'ParrillaController@index')->name('parrilla');
+
     Route::get('vertical2', 'Vertical2Controller@index')->name('vertical2');
 
     Route::get('vertical', 'VerticalController@index')->name('vertical');
