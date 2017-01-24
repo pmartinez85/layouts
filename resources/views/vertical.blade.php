e3e0b5c614ea58b370454f87891c68b9321a831a
Pedro Martinez <pmartinez1085@gmail.com>
2017-01-24 16:07:37 +0100

    vertical color

--- resources/views/vertical.blade.php
+++ resources/views/vertical.blade.php
@@ -1,21 +1,23 @@
-@extends('adminlte::page')
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
 
-@section('htmlheader_title')
-	Change Title here!
-@endsection
+<style>
+	body {
 
+		background: green;
+        color: white;
+	}
 
-@section('main-content')
-	<div class="container-fluid spark-screen">
-		<div class="row">
-			<div class="col-md-8 col-md-offset-2">
-				<div class="panel panel-default">
-					<div class="panel-heading">Change Header</div>
-					<div class="panel-body">
-						Change content here!
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-@endsection
+</style>
+
+<body>
+hello world
+</body>
+</html>
